4069f78c99babec93c105f5340303e371e01658a
Zaini-Aljury <Zaini.Al-Jufry@sg.ey.com>
2020-07-06 23:59:50 +0800

    Connect database to app

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import 'react-dates/initialize'
+import {startSetExpenses} from './actions/expenses'
+import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-import './firebase/firebase'
+import './firebase/firebase';
 
 const store = configureStore();
 
@@ -18,4 +19,8 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(()=>{
+  ReactDOM.render(jsx, document.getElementById('app'));
+})
