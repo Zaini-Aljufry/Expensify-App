783e09ab5295245eb74304dedb69a90048120d87
Zaini-Aljury <Zaini.Al-Jufry@sg.ey.com>
2020-07-06 14:36:22 +0800

    prepare for heroku

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+const pubDir = path.join(__dirname,'..','public')
+const port = process.env.PORT || 4000;
+
+app.use(express.static(pubDir));
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(pubDir, 'index.html'))
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listening to port ${port}`)
+})
